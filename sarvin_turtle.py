89770abdeca54af065d88348c66d505757e10f78
kishorpan2 <48890327+kishorpan2@users.noreply.github.com>
2024-03-03 12:28:34 -0800

    Create sarvin_turtle.py

---
+++ sarvin_turtle.py
@@ -0,0 +1,34 @@
+import turtle
+s=turtle.getscreen()
+y=turtle.Turtle()
+y.shape('turtle')
+y.color('blue')
+y.speed()
+y.begin_fill()
+y.fillcolor('orange')
+# Draw a square with 200 px 
+for a in range(4):
+    y.forward(200)
+    y.right(90)
+    y.forward(200)
+
+# Draw a triangle
+
+y.forward(150)
+y.right(120)
+y.forward(150)
+y.right(120)
+y.forward(150)
+
+# Draw a circle with radius 50 px
+y.circle(50)
+y.end_fill()
+
+# Draw a self writing line
+for a in range(40):
+    y.forward(50+10*a)
+    y.right(90)
+
+y.end_fill()
+y.hideturtle()
+turtle.exitonclick()
